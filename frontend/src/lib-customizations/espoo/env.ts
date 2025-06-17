aad02a8081ff6baa622fe2777d7d0a9b5c801600
Sami Oksanen <sami.oksanen@nordhero.com>
2025-06-17 15:47:41 +0300

    feat: set environment labels and feature flags

--- frontend/src/lib-customizations/espoo/env.ts
+++ frontend/src/lib-customizations/espoo/env.ts
@@ -2,15 +2,19 @@
 //
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
-export type Env = 'staging' | 'prod'
+export type Env = 'dev' | 'test' | 'prod'
 
 export const env = (): Env | 'default' => {
-  if (window.location.host === 'espoonvarhaiskasvatus.fi') {
+  if (window.location.host === 'evaka.petajavesi.fi') {
     return 'prod'
   }
 
-  if (window.location.host === 'staging.espoonvarhaiskasvatus.fi') {
-    return 'staging'
+  if (window.location.host === 'evaka-test.petajavesi.fi') {
+    return 'test'
+  }
+
+  if (window.location.host === 'evaka-dev.petajavesi.fi') {
+    return 'dev'
   }
 
   return 'default'
