a8c716505c14650b9bd4063b1b8997cafa21572d
PetteriSalenius <petteri.salenius@petajavesi.fi>
2025-08-21 13:50:04 +0300

    Update:  ServiceApplication enable

--- frontend/src/lib-customizations/espoo/featureFlags.tsx
+++ frontend/src/lib-customizations/espoo/featureFlags.tsx
@@ -182,7 +182,7 @@ const features: Features = {
     nekkuIntegration: false,
     forceUnpublishDocumentTemplate: false,
     invoiceDisplayAccountNumber: true,
-    serviceApplications: false,
+    serviceApplications: true,
     multiSelectDeparture: true,
     archiveIntegration: {
       childDocuments: true
