016727971790087908919eccf40ec5a4e220f8f6
PetteriSalenius <petteri.salenius@petajavesi.fi>
2025-09-29 21:58:44 +0300

    update: FinanceDesision nightly run disable

--- service/src/main/kotlin/fi/espoo/evaka/shared/job/ScheduledJobs.kt
+++ service/src/main/kotlin/fi/espoo/evaka/shared/job/ScheduledJobs.kt
@@ -117,7 +117,7 @@ enum class ScheduledJob(
     ),
     GenerateFinanceDecisions(
         ScheduledJobs::generateFinanceDecisions,
-        ScheduledJobSettings(enabled = true, schedule = JobSchedule.nightly()),
+        ScheduledJobSettings(enabled = false, schedule = JobSchedule.nightly()),
     ),
     KoskiUpdate(
         ScheduledJobs::koskiUpdate,
