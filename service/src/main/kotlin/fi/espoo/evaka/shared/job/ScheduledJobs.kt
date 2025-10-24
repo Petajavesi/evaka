eea362b6a87b92e99350a7413ebbe4b8ba99486a
PetteriSalenius <petteri.salenius@petajavesi.fi>
2025-10-24 12:53:35 +0300

    Update: Finance decision nightly cron enable

--- service/src/main/kotlin/fi/espoo/evaka/shared/job/ScheduledJobs.kt
+++ service/src/main/kotlin/fi/espoo/evaka/shared/job/ScheduledJobs.kt
@@ -114,7 +114,7 @@ enum class ScheduledJob(
     ),
     GenerateFinanceDecisions(
         ScheduledJobs::generateFinanceDecisions,
-        ScheduledJobSettings(enabled = false, schedule = JobSchedule.nightly()),
+        ScheduledJobSettings(enabled = true, schedule = JobSchedule.nightly()),
     ),
     KoskiUpdate(
         ScheduledJobs::koskiUpdate,
